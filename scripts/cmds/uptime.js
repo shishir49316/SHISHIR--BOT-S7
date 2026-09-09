0d2e37beb7f5dcf181067abe3582a6c991b38226
shishir49316 <sajusorkar49316426@gmail.com>
2026-09-09 11:13:00 +0600

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -124,7 +124,7 @@ async function generateUptimeImage(data) {
   
   ctx.font = '18px "Arial"';
   ctx.fillStyle = 'rgba(255,255,255,0.6)';
-  ctx.fillText('GOAT BOT V2•PREMIUM EDITION', width - 280, 55);
+  ctx.fillText('AY-MA BOT s7•PREMIUM EDITION', width - 280, 55);
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
   ctx.shadowBlur = 5;
@@ -324,7 +324,7 @@ async function generateUptimeImage(data) {
   ctx.fillRect(0, height - 40, width, 40);
   ctx.font = '13px "Arial"';
   ctx.fillStyle = 'rgba(255,255,255,0.5)';
-  ctx.fillText('© Create by xalman | All Systems Operational', 50, height - 15);
+  ctx.fillText('© Create by shishir | All Systems Operational', 50, height - 15);
   ctx.fillText(`Generated: ${moment().format('HH:mm:ss')}`, width - 200, height - 15);
   
   ctx.globalAlpha = 0.05;
