3de47fe7e596f0d93de4e8a5ef341e88936e8c59
shishir49316 <sajusorkar49316426@gmail.com>
2026-09-09 10:43:43 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -14,9 +14,9 @@ module.exports = {
 
   langs: {
     en: {
-      current: `📌 𝐆𝐎𝐀𝐓-𝐁𝐎𝐓
+      current: `📌𝘼𝙔-𝙈𝘼-𝐁𝐎𝐓
 ━━━━━━━━━━━━━━━━━━━━━━━━
-👑 𝐜𝐨𝐧𝐭𝐫𝐢𝐛𝐮𝐭𝐨𝐫 : NΞGΛTIVΞ XΛLMΛN
+👑 𝐜𝐨𝐧𝐭𝐫𝐢𝐛𝐮𝐭𝐨𝐫 :𝐀𝐡𝐦𝐞𝐃’𝐳 𝐒𝐇𝐈𝐒𝐇𝐈𝐑 
 🔗 𝐫𝐞𝐩𝐨𝐬𝐢𝐭𝐨𝐫𝐲      : %1
 💎 𝐬𝐭𝐚𝐭𝐮𝐬   : 𝐚𝐥𝐰𝐚𝐲𝐬 𝐮𝐩𝐝𝐚𝐭𝐢𝐧𝐠
 ━━━━━━━━━━━━━━━━━━━━━━━━`
@@ -24,13 +24,13 @@ module.exports = {
   },
 
   onStart: async function ({ message, getLang }) {
-    const link = "https://github.com/goatbotnx/GOAT-BOT-UPDATED";
+    const link = "-)শিশির বসের প্যান্টের নিচে আসে  চুষে দাও আর fork নিয়ে যাও🙂🐸";
     return message.reply(getLang("current", link));
   },
 
   onChat: async function ({ message, getLang, event }) {
     if (event.body && event.body.toLowerCase() === "fork") {
-      const link = "https://github.com/goatbotnx/GOAT-BOT-UPDATED";
+      const link = "aiw aktu chuse de amar ta vid..🍌🍌🍌 dibo ne....🚷";
       return message.reply(getLang("current", link));
     }
   }
